6f7664734111b3a09070bc5fd3b929750ae54bc8
Sasha Khirov <sasha.khiro@gmail.com>
2023-11-25 18:47:05 +0200

    added new  method for get lands by id

--- contracts/LandParameters.sol
+++ contracts/LandParameters.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.9;
 
 contract LandParameters {
-   struct LandArray {
+    struct LandArray {
         uint256 landId;
-        string info; 
+        string info;
     }
 
     LandArray[] public landConnections;
 
-    function createConnection(uint256 _landId, string memory _info) public {
+    function createParameter(uint256 _landId, string memory _info) public {
         LandArray memory newConnection = LandArray(_landId, _info);
         landConnections.push(newConnection);
     }
@@ -18,9 +18,33 @@ contract LandParameters {
         return landConnections.length;
     }
 
-    function getConnection(uint256 index) public view returns (LandArray memory) {
+    function getConnection(
+        uint256 index
+    ) public view returns (LandArray memory) {
         require(index < landConnections.length, "Index out of bounds");
         return landConnections[index];
     }
 
+    function getAllObjectsByLandId(
+        uint256 _landId
+    ) public view returns (LandArray[] memory) {
+        LandArray[] memory filteredObjects = new LandArray[](
+            landConnections.length
+        );
+
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < landConnections.length; i++) {
+            if (landConnections[i].landId == _landId) {
+                filteredObjects[count] = landConnections[i];
+                count++;
+            }
+        }
+
+        assembly {
+            mstore(filteredObjects, count)
+        }
+
+        return filteredObjects;
+    }
 }
