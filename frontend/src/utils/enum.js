6619850201b8bf76d33f3a8018234e6f4c80bb61
Sasha Khirov <sasha.khiro@gmail.com>
2023-11-29 00:35:31 +0200

    created enum for create form

---
+++ frontend/src/utils/enum.js
@@ -0,0 +1,22 @@
+export const LandTypes = {
+  AGRICULTURAL: 0,
+  SETTLEMENT: 1,
+  INDUSTRIAL_COMMERCIAL: 2,
+  RECREATIONAL: 3,
+  CONSTRUCTION: 4,
+  WATER_FUND: 5,
+  FOREST_FUND: 6,
+  SPECIAL_PURPOSE: 7,
+};
+
+export const LandTypeLabels = {
+  [LandTypes.AGRICULTURAL]: "Земля сільськогосподарського призначення",
+  [LandTypes.SETTLEMENT]: "Земля населеного пункта",
+  [LandTypes.INDUSTRIAL_COMMERCIAL]:
+    "Земля промислового та комерційного призначення",
+  [LandTypes.RECREATIONAL]: "Земля рекреаційного призначення",
+  [LandTypes.CONSTRUCTION]: "Земля для будівництва",
+  [LandTypes.WATER_FUND]: "Земля водного фонду",
+  [LandTypes.FOREST_FUND]: "Земля лісового фонду",
+  [LandTypes.SPECIAL_PURPOSE]: "Земля об'єктів та спеціального призначення",
+};
