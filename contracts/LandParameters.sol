4187cb3d4df726a6e89dd603310e7e2d38b5573d
Sasha Khirov <sasha.khiro@gmail.com>
2023-12-01 18:15:04 +0200

    updateing logick in structure LandParameters

--- contracts/LandParameters.sol
+++ contracts/LandParameters.sol
@@ -5,26 +5,93 @@ contract LandParameters {
     struct LandArray {
         uint256 id;
         uint256 landId;
-        string info;
+        uint256 dateTime;
+        string description;
+        address owner;
+        // New fields
+        uint256 pHLevel;
+        uint256 organicMatter;
+        uint256 nitrogenContent;
+        uint256 phosphorusContent;
+        uint256 potassiumContent;
+        uint256 area;
     }
 
     LandArray[] public landConnections;
     uint256 private objectIdCounter = 1;
 
-    function createParameter(uint256 _landId, string memory _info) public {
-        addObject(_info, _landId);
+    function createParameter(
+        uint256 _landId,
+        string memory _description,
+        uint256 _pHLevel,
+        uint256 _organicMatter,
+        uint256 _nitrogenContent,
+        uint256 _phosphorusContent,
+        uint256 _potassiumContent,
+        uint256 _area
+    ) public {
+        addObject(
+            _landId,
+            _description,
+            block.timestamp,
+            _pHLevel,
+            _organicMatter,
+            _nitrogenContent,
+            _phosphorusContent,
+            _potassiumContent,
+            _area
+        );
     }
 
-    function addObject(string memory _info, uint256 _landId) private {
-        LandArray memory newObject = LandArray(objectIdCounter, _landId, _info);
+    function addObject(
+        uint256 _landId,
+        string memory _description,
+        uint256 _timestamp,
+        uint256 _pHLevel,
+        uint256 _organicMatter,
+        uint256 _nitrogenContent,
+        uint256 _phosphorusContent,
+        uint256 _potassiumContent,
+        uint256 _area
+    ) private {
+        LandArray memory newObject = LandArray(
+            objectIdCounter,
+            _landId,
+            _timestamp,
+            _description,
+            msg.sender, // assuming you want to set the owner as the function caller
+            _pHLevel,
+            _organicMatter,
+            _nitrogenContent,
+            _phosphorusContent,
+            _potassiumContent,
+            _area
+        );
         landConnections.push(newObject);
         objectIdCounter++;
     }
 
-    function editObject(uint256 _id, string memory _info) public {
+    function editObject(
+        uint256 _id,
+        string memory _description,
+        uint256 _pHLevel,
+        uint256 _organicMatter,
+        uint256 _nitrogenContent,
+        uint256 _phosphorusContent,
+        uint256 _potassiumContent,
+        uint256 _area
+    ) public {
         for (uint256 i = 0; i < landConnections.length; i++) {
             if (landConnections[i].id == _id) {
-                landConnections[i].info = _info;
+                landConnections[i].description = _description;
+                landConnections[i].dateTime = block.timestamp;
+                landConnections[i].pHLevel = _pHLevel;
+                landConnections[i].organicMatter = _organicMatter;
+                landConnections[i].nitrogenContent = _nitrogenContent;
+                landConnections[i].phosphorusContent = _phosphorusContent;
+                landConnections[i].potassiumContent = _potassiumContent;
+                landConnections[i].area = _area;
+
                 break;
             }
         }
@@ -53,13 +120,19 @@ contract LandParameters {
         return landConnections.length;
     }
 
-    function getConnection(uint256 index) public view returns (LandArray memory) {
+    function getConnection(
+        uint256 index
+    ) public view returns (LandArray memory) {
         require(index < landConnections.length, "Index out of bounds");
         return landConnections[index];
     }
 
-    function getAllObjectsByLandId(uint256 _landId) public view returns (LandArray[] memory) {
-        LandArray[] memory filteredObjects = new LandArray[](landConnections.length);
+    function getAllObjectsByLandId(
+        uint256 _landId
+    ) public view returns (LandArray[] memory) {
+        LandArray[] memory filteredObjects = new LandArray[](
+            landConnections.length
+        );
 
         uint256 count = 0;
 
