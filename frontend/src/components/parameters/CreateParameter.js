774742b79793b67941b08ecf5c705f75aa79f20f
KhirovSasha <sasha.khiro@gmail.com>
2023-12-09 11:57:30 +0200

    added correct create page for ladn parameter

--- frontend/src/components/parameters/CreateParameter.js
+++ frontend/src/components/parameters/CreateParameter.js
@@ -1,27 +1,40 @@
 import { useParams, useNavigate } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
 export function CreateParameter({ parametersAt }) {
+  const [formData, setFormData] = useState({
+    desctiption: '',
+    pHLevel: 0,
+    organicMatter: 0,
+    nitrogenContent: 0,
+    phosphorusContent: 0,
+    potassiumContent: 0,
+    area: 0
+  });
+  
+
   const [contractValue, setContractValue] = useState("");
   const { id } = useParams();
   const navigate = useNavigate();
+  
 
   const handleSubmit = async (event) => {
-    event.preventDefault(); 
-
-    console.log("Contract Value:", contractValue);
+    event.preventDefault();
     try {
       const solidityNumber = ethers.BigNumber.from(id);
-      await parametersAt.createParameter(solidityNumber, String(contractValue));
+      await parametersAt.createParameter(solidityNumber, formData.desctiption, String(formData.pHLevel), String(formData.organicMatter), String(formData.nitrogenContent), String(formData.phosphorusContent), String(formData.potassiumContent), String(formData.area));
       navigate(`/land-parameters/${parseInt(id, 10)}`);
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
-  const handleChange = (event) => {
-    setContractValue(event.target.value);
+  const handleChange = (propertyName) => (event) => {
+    setFormData({
+      ...formData,
+      [propertyName]: event.target.value,
+    });
   };
 
   return (
@@ -29,12 +42,57 @@ export function CreateParameter({ parametersAt }) {
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="value" class="form-label">
-            Value of contract
+            description
           </label>
           <input
             className="form-control"
-            value={contractValue}
-            onChange={handleChange}
+            value={formData.desctiption}
+            onChange={handleChange("desctiption")}
+          ></input>
+          <label htmlFor="value" class="form-label">
+            pHLevel
+          </label>
+          <input
+            className="form-control"
+            value={formData.pHLevel}
+            type="number"
+            onChange={handleChange("pHLevel")}
+          ></input>
+          <label htmlFor="value" class="form-label">
+            organicMatter
+          </label>
+          <input
+            className="form-control"
+            value={formData.organicMatter}
+            type="number"
+            onChange={handleChange("organicMatter")}
+          ></input>
+          <label htmlFor="value" class="form-label">
+            nitrogenContent
+          </label>
+          <input
+            className="form-control"
+            value={formData.nitrogenContent}
+            type="number"
+            onChange={handleChange("nitrogenContent")}
+          ></input>
+          <label htmlFor="value" class="form-label">
+            phosphorusContent
+          </label>
+          <input
+            className="form-control"
+            value={formData.phosphorusContent}
+            type="number"
+            onChange={handleChange("phosphorusContent")}
+          ></input>
+          <label htmlFor="value" class="form-label">
+            potassiumContent
+          </label>
+          <input
+            className="form-control"
+            value={formData.potassiumContent}
+            type="number"
+            onChange={handleChange("potassiumContent")}
           ></input>
         </div>
         <button type="submit" className="btn btn-primary">
